a6a003ffa2d2e46dbd7049c4b1db0d38d0eae4e4
Eric Dunford <eric.dunford@georgetown.edu>
2019-06-24 12:20:45 -0500

    Bones of the basic data structure

---
+++ Data/example_log_data.R
@@ -0,0 +1,6 @@
+# Create a fake example data structure (bones)
+d_str = list(variable=list(coding=-99,doc_type=NA,reliab=-99,internal_ref=NA,author=NA,
+    source=NA,page_no=NA,year=NA,evidence_entry="",entry_note="",timestamp=NA))
+
+d_str 
+
