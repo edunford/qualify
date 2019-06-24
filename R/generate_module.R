018516e7a12d4ff547a0d203c82879c5942e7e4e
Eric Dunford <eric.dunford@georgetown.edu>
2019-06-24 15:24:01 -0500

    push initial scoping of the generate_module() method

---
+++ R/generate_module.R
@@ -0,0 +1,70 @@
+# qual_build()
+
+# Build the input requests for the basic input scheme. 
+
+
+#' generate_module()
+#' 
+#' Build the input requests for the basic input scheme. 
+#'
+#' @param variable_name 
+#' @param ... 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+# generate_module = function(variable_name = "",
+#                            caption = "",
+#                            ...){
+#   UseMethod("generate_module")
+# }
+
+
+# @export 
+generate_module = function(variable_name = "",
+                           caption = "",
+                           ...){
+  obj = list(x = list(...))
+  names(obj) = variable_name
+  return(list(caption = caption,construction = obj, fields = sapply(obj[[1]],class)))
+}
+
+
+#' date_field
+#'
+#' Placeholder for date field entry parameter on the application module. 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+date_field = function(placehold_date = "1900-01-01"){
+  as.Date(placehold_date)
+}
+
+
+#' text_field
+#'
+#' Placeholder for text field entry parameter on the application module. 
+#'
+#' @param txt 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+text_field = function(txt = ""){
+  txt  
+}
+
+
+# Test
+generate_module(variable_name = "var_1",
+                caption = "",
+                this = text_field(), 
+                date_field = date_field(), 
+                list = c(1,2,3,4))
+
+
+
