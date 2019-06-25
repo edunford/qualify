7694dcd2945ddb5e0a67760ea7cfc4e1baa3160e
Eric Dunford <eric.dunford@georgetown.edu>
2019-06-25 17:06:46 -0500

    Alter App.js to read only compiled app code (from the functions)

--- user_interface/src/App.js
+++ user_interface/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
-import { PostEdit } from './posts';
+import { PostEdit } from './posts_compiled';
 
 const App = () => (
     <Admin dataProvider={jsonServerProvider('http://localhost:8000')}>
