ae90f78d19750ec73b4d2687ab6ceca96efe6b16
Nicholas Hagar <nicholasrhagar@gmail.com>
2019-06-25 10:10:49 -0500

    updated default view to form

--- user_interface/src/App.js
+++ user_interface/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Redirect, Switch } from 'react-router'
 import { Admin, Resource } from 'react-admin';
 import simpleRestProvider from 'ra-data-simple-rest';
 
@@ -7,6 +8,7 @@ import { PostCreate } from './posts';
 const App = () => (
     <Admin dataProvider={simpleRestProvider('http://localhost:8000')}>
         <Resource name="posts" create={PostCreate} />
+        <Redirect from="/" to="/posts/create" />
     </Admin>
 );
 
