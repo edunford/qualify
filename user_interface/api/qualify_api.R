0ff0197553ae4d29089e3434c27f4e0b136c49ea
Eric Dunford <eric.dunford@georgetown.edu>
2019-06-26 15:36:41 -0500

    Created functionality to point package database to a user-specified project folder.

--- user_interface/api/qualify_api.R
+++ user_interface/api/qualify_api.R
@@ -3,97 +3,8 @@
 source(here::here("R/qualify.R"))
 require(tidyverse)
 
-
-# Main Function Calls -----------------------------------------------------
-
-
-#' import_data_state
-#'
-#' @param .project_name initial name of the data_base build.
-#'
-#' @return exports all units along with the current state of the projec
-import_data_state <- function(.project_name = "test_db",empty_value_placeholder=""){
-  con = sql_instance(.project_name) 
-  all_tbls = grep("v",dplyr::src_tbls(con),value = T)
-  report = c()
-  for ( t in all_tbls){
-    report <-
-      tbl(con,t) %>% 
-      collect() %>% 
-      group_by(.unit) %>% 
-      arrange(desc(timestamp)) %>% 
-      slice(1) %>% 
-      ungroup %>% 
-      select(-timestamp) %>% 
-      bind_rows(report,.)
-  }
-  
-  # return the data state with a progress report
-  report %>% 
-    group_by(.unit) %>% 
-    nest() %>% 
-    mutate(progress = map(data,function(x) sum(rowSums(x == empty_value_placeholder) < ncol(x))/nrow(x))) %>% 
-    unnest(progress) %>% 
-    select(id = .unit, Progress = progress)
-}
-
-
-#' api_data_call
-#'
-#' @param unit 
-#' @param .project_name 
-#'
-#' @return
-api_data_call = function(unit = "",.project_name = "test_db"){
-  con = sql_instance(.project_name)
-  all_tbls = grep("v",dplyr::src_tbls(con),value = T)
-  api_order <- c()
-  for(t in all_tbls){
-    dplyr::tbl(con,t) %>%
-      dplyr::filter(.unit == unit) %>%
-      dplyr::arrange(desc(timestamp)) %>% 
-      dplyr::select(-.unit) %>% 
-      dplyr::collect() %>% 
-      dplyr::slice(1) %>% 
-      {colnames(.) = paste0(t,"_",colnames(.));.} %>% 
-      dplyr::bind_cols(api_order,.) -> api_order
-  }
-  api_order$id = unit
-  return(as.data.frame(api_order)) # Send back the request
-}
-
-
-#' upload_data
-#'
-#' @param entry 
-#' @param .project_name 
-#'
-#' @return
-upload_data = function(entry,.project_name = "test_db"){
-  # Process entry records for resubmission into the data element. 
-  id = entry$id 
-  entry["id"] = NULL
-  entry["Progress"] = NULL
-  tags = stringr::str_remove_all(stringr::str_extract_all(names(entry),"v\\d_",simplify = T),"_") 
-  vars = stringr::str_remove_all(names(entry),"v\\d_")
-  to_record <- 
-    tibble::tibble(.tag = tags,vars,entry = unlist(entry,use.names = F)) %>% 
-    tidyr::spread(vars,entry) %>% 
-    dplyr::mutate(.unit=id)
-  
-  # Loop through tags and draw out relevant features
-  for(t in to_record$.tag){
-    tmp = to_record %>% 
-      dplyr::filter(.tag == t) %>%
-      dplyr::select(-.tag) %>% 
-      dplyr::mutate(timestamp = as.character(Sys.time()))
-    
-    # Append to existing data frame
-    con = sql_instance(.project_name)
-    DBI::dbWriteTable(conn=con$con, name = t, value = tmp,append=T) 
-  }
-}
-
+# TEMPORARY
+db_location <- "~/Desktop/test_project/"
 
 # Plumber API Features ----------------------------------------------------
 
@@ -116,7 +27,7 @@ cors <- function(req, res) {
 #* A test endpoint for providing dummy data
 #' @get /posts
 function(req, res){
-  master_data <- import_data_state()
+  master_data <- import_data_state(.project_path = db_location)
   res$setHeader("Access-Control-Expose-Headers", "X-Total-Count")
   res$setHeader("X-Total-Count", nrow(master_data))
   master_data
@@ -125,12 +36,12 @@ function(req, res){
 #* An endpoint for getting specific records
 #' @get /posts/<pid>
 function(pid){
-  jsonlite::unbox(api_data_call(unit = pid))
+  jsonlite::unbox(api_data_call(unit = pid,.project_path = db_location))
 }
 
 #' @put /posts/<pid>
 function(req){
   entry = jsonlite::fromJSON(req$postBody)
-  upload_data(entry)
+  upload_data(entry,.project_path = db_location)
   jsonlite::fromJSON(req$postBody)
 }
